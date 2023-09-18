8e6bff369a8194d7074547eae5f4172e0e0b11ab
Krushangini <krushanginis32gmail.com>
2023-09-18 09:34:55 +0530

    final

--- PensionCalculator.java
+++ PensionCalculator.java
@@ -2,66 +2,58 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PensionCalculator extends JFrame {
     private JTextField dobEntry, dojEntry, retEntry, basicEntry, daEntry, hraEntry, ccaEntry, maEntry;
     private JLabel pensionResultLabel, commutationResultLabel, pafCommutationResultLabel, elEncashmentResultLabel;
+    CardLayout cardLayout = new CardLayout();
+    JPanel cardPanel = new JPanel(cardLayout);
 
-    public PensionCalculator() {
+     public PensionCalculator() {
         setTitle("Pension Calculator");
-        setSize(700, 800);
+        setSize(700, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(8, 2, 5, 5));
 
-        addLabelAndInput(panel, "Date of Birth (dd/mm/yyyy):", dobEntry = new JTextField());
-        addLabelAndInput(panel, "Date of Joining (dd/mm/yyyy):", dojEntry = new JTextField());
-        addLabelAndInput(panel, "Retirement Date (dd/mm/yyyy):", retEntry = new JTextField());
-        addLabelAndInput(panel, "Basic pay on retirement:", basicEntry = new JTextField());
-        addLabelAndInput(panel, "DA on retirement:", daEntry = new JTextField());
-        addLabelAndInput(panel, "HRA on retirement:", hraEntry = new JTextField());
-        addLabelAndInput(panel, "CCA on retirement:", ccaEntry = new JTextField());
-        addLabelAndInput(panel, "MA on retirement:", maEntry = new JTextField());
+        JPanel homePanel = new JPanel();
+        homePanel.setLayout(new GridLayout(20, 1, 5, 5));
 
+        addLabelAndInput(homePanel, "Date of Birth (dd/mm/yyyy):", dobEntry = new JTextField());
+        addLabelAndInput(homePanel, "Date of Joining (dd/mm/yyyy):", dojEntry = new JTextField());
+        addLabelAndInput(homePanel, "Retirement Date (dd/mm/yyyy):", retEntry = new JTextField());
+        addLabelAndInput(homePanel, "Basic pay on retirement:", basicEntry = new JTextField());
+        addLabelAndInput(homePanel, "DA on retirement:", daEntry = new JTextField());
+        addLabelAndInput(homePanel, "HRA on retirement:", hraEntry = new JTextField());
+        addLabelAndInput(homePanel, "CCA on retirement:", ccaEntry = new JTextField());
+        addLabelAndInput(homePanel, "MA on retirement:", maEntry = new JTextField());
 
+        JButton calculateButton = new JButton("Calculate");
+        calculateButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    calculatePension();
+                } catch (ParseException parseException) {
+                    parseException.printStackTrace();
+                }
+            }
+        });
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                clearFields();
+            }
+        });
 
-
-       // Create the "Calculate" button
-JButton calculateButton = new JButton("Calculate");
-calculateButton.addActionListener(new ActionListener() {
-    public void actionPerformed(ActionEvent e) {
-        calculatePension();
-    }
-});
-
-// Set the preferred and minimum size for the "Calculate" button
-calculateButton.setPreferredSize(new Dimension(100, 30));
-calculateButton.setMinimumSize(new Dimension(100, 30));
-
-// Create the "Clear" button
-JButton clearButton = new JButton("Clear");
-clearButton.addActionListener(new ActionListener() {
-    public void actionPerformed(ActionEvent e) {
-        clearFields();
-    }
-});
-
-// Set the preferred and minimum size for the "Clear" button
-clearButton.setPreferredSize(new Dimension(100, 30));
-clearButton.setMinimumSize(new Dimension(100, 30));
-
-
-        panel.add(calculateButton);
-        panel.add(clearButton);
+        homePanel.add(calculateButton);
+        homePanel.add(clearButton);
 
         JPanel resultPanel = new JPanel();
-        resultPanel.setLayout(new GridLayout(4, 2, 10, 20));
+        resultPanel.setLayout(new GridLayout(8, 1));
 
         pensionResultLabel = new JLabel("Full Pension:");
         pensionResultLabel.setFont(new Font("Arial", Font.BOLD, 16));
@@ -71,6 +63,13 @@ clearButton.setMinimumSize(new Dimension(100, 30));
         pafCommutationResultLabel.setFont(new Font("Arial", Font.BOLD, 16));
         elEncashmentResultLabel = new JLabel("EL Encashment:");
         elEncashmentResultLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        JButton back = new JButton("Back");
+
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                cardLayout.show(cardPanel,"home");
+            }
+        });
 
         resultPanel.add(pensionResultLabel);
         resultPanel.add(new JLabel());
@@ -80,10 +79,14 @@ clearButton.setMinimumSize(new Dimension(100, 30));
         resultPanel.add(new JLabel());
         resultPanel.add(elEncashmentResultLabel);
         resultPanel.add(new JLabel());
+        resultPanel.add(back);
 
-        panel.add(resultPanel);
+        cardPanel.add(homePanel, "home");
+        cardPanel.add(resultPanel, "result");
 
-        add(panel);
+        cardLayout.show(cardPanel, "home");
+
+        add(cardPanel);
         setVisible(true);
     }
 
@@ -101,37 +104,35 @@ clearButton.setMinimumSize(new Dimension(100, 30));
         panel.add(row);
     }
 
-    private void calculatePension() {
+    private void calculatePension() throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        try {
-            Date dob = dateFormat.parse(dobEntry.getText());
-            Date doj = dateFormat.parse(dojEntry.getText());
-            Date dor = dateFormat.parse(retEntry.getText());
+        Date dob = dateFormat.parse(dobEntry.getText());
+        Date doj = dateFormat.parse(dojEntry.getText());
+        Date dor = dateFormat.parse(retEntry.getText());
 
-            // Perform the pension calculations here
-            int serviceYears = (int) ((dor.getTime() - doj.getTime()) / (1000 * 60 * 60 * 24 * 365.25));
-            int basic = Integer.parseInt(basicEntry.getText());
-            double fullPension = (basic / 2.0) * (serviceYears / 60.0);
-            double oneThirdPension = fullPension / 3.0;
-            double pensionAfterCommutation = fullPension - oneThirdPension;
-            double da = Double.parseDouble(daEntry.getText());
-            double hra = Double.parseDouble(hraEntry.getText());
-            double cca = Double.parseDouble(ccaEntry.getText());
-            double ma = Double.parseDouble(maEntry.getText());
-            int retMonth = dor.getMonth() + 1;
-            int daysInMonth = getDaysInMonth(retMonth, dor.getYear() + 1900);
-            double payPerDay = (basic + da + hra + cca + ma) / daysInMonth;
-            double elEncashment = 240 * payPerDay;
+        // Perform the pension calculations here
+        int serviceYears = (int) ((dor.getTime() - doj.getTime()) / (1000 * 60 * 60 * 24 * 365.25));
+        int basic = Integer.parseInt(basicEntry.getText());
+        double fullPension = (basic / 2.0) * (serviceYears / 60.0);
+        double oneThirdPension = fullPension / 3.0;
+        double pensionAfterCommutation = fullPension - oneThirdPension;
+        double da = Double.parseDouble(daEntry.getText());
+        double hra = Double.parseDouble(hraEntry.getText());
+        double cca = Double.parseDouble(ccaEntry.getText());
+        double ma = Double.parseDouble(maEntry.getText());
+        int retMonth = dor.getMonth() + 1;
+        int daysInMonth = getDaysInMonth(retMonth, dor.getYear() + 1900);
+        double payPerDay = (basic + da + hra + cca + ma) / daysInMonth;
+        double elEncashment = 240 * payPerDay;
 
-            // Update the result labels accordingly
-            pensionResultLabel.setText("Full Pension: " + String.format("%.2f", fullPension));
-            commutationResultLabel.setText("Commutation Amount: " + String.format("%.2f", oneThirdPension));
-            pafCommutationResultLabel.setText("Pension after Commutation: " + String.format("%.2f", pensionAfterCommutation));
-            elEncashmentResultLabel.setText("EL Encashment: " + String.format("%.2f", elEncashment));
+        // Update the result labels accordingly
+        pensionResultLabel.setText("Full Pension: " + String.format("%.2f", fullPension));
+        commutationResultLabel.setText("Commutation Amount: " + String.format("%.2f", oneThirdPension));
+        pafCommutationResultLabel.setText("Pension after Commutation: " + String.format("%.2f", pensionAfterCommutation));
+        elEncashmentResultLabel.setText("EL Encashment: " + String.format("%.2f", elEncashment));
 
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(this, "Invalid input or date format (dd/mm/yyyy).", "Error", JOptionPane.ERROR_MESSAGE);
-        }
+        // Switch to the result panel
+        cardLayout.show(cardPanel, "result");
     }
 
     private void clearFields() {
